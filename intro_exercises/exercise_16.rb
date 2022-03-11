b3137d530d531531bacb526cb93c618b2edb4e84
Jondruh <hurd.jonathan@gmail.com>
2022-03-11 14:05:20 -0600

    refactored to use #each_with_index and fields variable

--- intro_exercises/exercise_16.rb
+++ intro_exercises/exercise_16.rb
@@ -1,15 +1,14 @@
 contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
             ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 
-contacts = {"Joe Smith" => {email: "", address: "", phone: ""}, "Sally Johnson" => {email: "", address: "", phone: ""}}
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+fields = [:email, :address, :phone] 
 
-counter = 0
 
-contacts.each_key do |person|
-  contacts[person].each do |key, value|
-    contacts[person][key] = contact_data[counter].shift
+contacts.each_with_index do |(person, info), idx|
+  fields.each do |field|
+    info[field] = contact_data[idx].shift
   end
-  counter += 1
 end
 
 puts contacts["Joe Smith"]
